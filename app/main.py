06d8782ae582458764f77fb3d19a1fdfab318d50
Junqing <junqingqian008@gmail.com>
2022-11-05 17:50:39 +0100

    refine dummy endpoints templating, adding basic fibonacci function with local cache object

--- app/main.py
+++ app/main.py
@@ -1,23 +1,30 @@
+import logging
 from fastapi import FastAPI
+from .utils import fibonacci_of_func
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s :: %(levelname)s :: %(message)s'
+)
 
 app = FastAPI()
 
 
 @app.get("/fibonacci/{input}")
 async def fibonacci_index(input: int):
-    return {"data": 5}
+    return {"data": fibonacci_of_func(input)}
 
 
-@app.get("/fibonacci/{input}")
+@app.get("/fibonacci/sequence/{input}")
 async def fibonacci_sequence(input: int, page: int, pagesize: int = 100):
     return {"data": [5]}
 
 
-@app.post("/fibonacci/{input}")
+@app.post("/fibonacci/blacklist/{input}")
 async def add_blacklist(input: int):
     return {"ok"}
 
 
-@app.delete("/fibonacci/{input}")
+@app.delete("/fibonacci/blacklist/undo/{input}")
 async def remove_blacklist(input: int):
     return {"ok"}
