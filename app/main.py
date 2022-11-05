72560fd9cb82a621d063dc12b100146a781789e5
Junqing <junqingqian008@gmail.com>
2022-11-05 17:45:38 +0100

    Feat: enabling all four dummy endpoints in accordance to the assignment

--- app/main.py
+++ app/main.py
@@ -3,14 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/fibonacci")
+@app.get("/fibonacci/{input}")
 async def fibonacci_index(input: int):
-    """_summary_
-
-    Args:
-        input (int): _description_
-
-    Returns:
-        _type_: _description_
-    """
     return {"data": 5}
+
+
+@app.get("/fibonacci/{input}")
+async def fibonacci_sequence(input: int, page: int, pagesize: int = 100):
+    return {"data": [5]}
+
+
+@app.post("/fibonacci/{input}")
+async def add_blacklist(input: int):
+    return {"ok"}
+
+
+@app.delete("/fibonacci/{input}")
+async def remove_blacklist(input: int):
+    return {"ok"}
